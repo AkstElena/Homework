804789f0e22dd828adc682fdf98a98e3fd778476
Akst Elena <akstelena@yandex.ru>
2023-02-26 12:31:23 +0300

    C# Homework 1

---
+++ Homework01/Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа в промежутке от 1 до " + number + ":");
+for (int evenNumber = 2; evenNumber <= number; evenNumber=evenNumber+2)
+{
+  Console.Write(evenNumber + " ");
+}
