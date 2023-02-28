c12257384d38e4eccf10acae627ce9f02d6e8a6a
Akst Elena <akstelena@yandex.ru>
2023-02-28 13:57:52 +0300

    Second Homework

---
+++ Homework02/Task10/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Введите трехзначное чиcло:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 99 && number < 1000)
+//   Console.WriteLine("Вторая цифра введенного числа: " + (number/10)%10);
+// else Console.WriteLine("Введено некорретное число! Необходимо ввести трехзначное число");
+
+Console.WriteLine("Введите трехзначное чиcло:");
+string input = Console.ReadLine();
+int number;
+
+bool isNumeric = int.TryParse(input, out number);
+
+if (isNumeric)
+{
+  if (number > 99 && number < 1000)
+  {
+    char secondChar = input [1];
+    Console.WriteLine("Вторая цифра введенного числа: " + secondChar);
+  }
+  else Console.WriteLine("Введено некорретное число! Необходимо ввести трехзначное число");
+ }
+else Console.WriteLine("Необходимо вводить число, а не текст!");
---
+++ Homework02/Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+
+string input = Console.ReadLine();
+int number;
+
+bool isNumeric = int.TryParse(input, out number);
+
+if (isNumeric)
+{
+  if (number > 100)
+  {
+    char thirdChar = input [2];
+    Console.WriteLine("Третья цифра заданного числа: " + thirdChar);
+  }
+  else if (number < -99)
+  {
+    char thirdChar = input [3];
+    Console.WriteLine("Третья цифра заданного числа: " + thirdChar);
+  }
+  else Console.WriteLine("У данного числа нет третьей цифры");
+ }
+else Console.WriteLine("Необходимо вводить число, а не текст!");
